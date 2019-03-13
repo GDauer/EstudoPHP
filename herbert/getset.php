2c115f0fca14364b694decd8ff946ca2506baee5
Herbert Rosa <herbert.souza@webjump.com.br>
2019-03-13 14:35:07 -0300

    Segunda Atualização dos objetos

--- herbert/getset.php
+++ herbert/getset.php
@@ -1,1 +1,71 @@
-/srv/http/seudominio1.com/html/getset.php
+<?php
+
+class estudos {
+
+    private $name;
+    private $email;
+    private $idade;
+
+
+public  function _construct($name, $email ,$idade){
+
+    $this-> name = $name;
+    $this -> email =$email;
+    $this -> idade =$idade;
+
+}
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name = $_POST['name'];
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email = $_POST['email'];
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade = $_POST['idade'];
+    }
+
+    /**
+     * @param mixed $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+
+}
+
+
+    ?>
--- herbert/ifelse.php
+++ herbert/ifelse.php
@@ -1,1 +1,17 @@
-/srv/http/seudominio1.com/html/ifelse.php
+<?php
+
+ function checkidade($idade=0)
+{
+    if ($idade < 18) {
+
+        echo "<br> A idade é de uma criança ou de um adolescente";
+
+    } elseif ($idade >= 18 and $idade < 60) {
+        echo "<br>A idade é de um adulto";
+    } else {
+
+        echo "<br>A idade é de uma pessoa de terceira idade";
+
+    }
+}
+?>
