05f910d2d34cb17c4e0805d205d031e891dd7f9e
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-13 11:03:52 -0300

    Iniciando estudos PHP

---
+++ brenda/PHP/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> TEST </title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+    <?php
+        $nome = Brenda;
+        $idade = 20;
+        echo "$nome tem $idade anos!";
+    ?>
+</body>    
+</html>
