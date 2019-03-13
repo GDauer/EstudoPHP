3dda3c9ad2d56295c6604f906ba82f9774ad306c
Brenda-barbosa <48325248+Brenda-barbosa@users.noreply.github.com>
2019-03-13 15:08:51 -0300

    Add files via upload

---
+++ brenda/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> TEST </title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <?php
+    $nome = Brenda;
+    $idade = 20;
+    $n1 = $_GET["a"];
+    $n2 = $_GET ["b"];
+    $m =($n1 + $n2)/2;
+    echo "$nome tem $idade anos e sua média de suas nota foi $m";
+    $sit = ($m < 6)?"VOCÊ FOI REPROVADO":"VOCÊ FOI APROVADO";
+    echo "</br> A situação atual do aluno é: $sit";
+    ?>
+</body>
+
+</html> 
