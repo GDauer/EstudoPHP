92a212b9c330df930ed7474128bc119a139d6844
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-13 12:14:12 -0300

    Usando a função  para pegar informações da URL

--- brenda/PHP/test.php
+++ brenda/PHP/test.php
@@ -10,19 +10,14 @@
     <?php
     $nome = Brenda;
     $idade = 20;
-    $n1 = 8;
-    $n2 = 7;
+    $n1 = $_GET["a"];
+    $n2 = $_GET ["b"];
     $a = $n1 + $n2;
     $b = $n1 - $n2;
-    $c = $n1 * $n2;
-    $d = $n1 / $n2;
-    $e = $n1 % $n2;
     echo "$nome tem $idade anos!";
     echo "<br/><br/> A soma das notas entre $n1 e $n2 é igual a: $a";
     echo "<br/>  A subtração das notas entre $n1 e $n2 é igual a: $b";
-    echo "<br/>  A multiplicação das notas entre $n1 e $n2 é igual a: $c";
-    echo "<br/>  A divisão das notas entre $n1 e $n2 é igual a: $d";
-    echo "<br/>  O modulo das notas entre $n1 e $n2 é igual a: $e";
+   
     ?>
 </body>
 
