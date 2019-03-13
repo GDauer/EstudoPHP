5efcabc2c600ebdaa7ae5f6dde47ddd77cbcaf47
Gustavo Dauer <gustavo.dauer@webjump.com.br>
2019-03-13 14:04:27 -0300

    Arquivos default para projeto adicionado a minha pasta!

---
+++ gustavo/test/somaTest.php
@@ -0,0 +1,19 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+
+class SomaTest extends TestCase
+{
+   public function testSoma()
+   {
+      $this->assertEquals(4, soma(2,2));
+   }
+
+   public function testSomaSemParametros()
+   {
+      $this->assertEquals(0, soma());
+   }
+}
+
+
+?>
