5691a8e5b755eb90a6933bc5d146f29e1a40e560
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-14 10:14:36 -0300

    Função  pegando inf da url

--- brenda/PHP/test.php
+++ brenda/PHP/test.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-       <title> TEST </title>
-       <meta charset="utf-8"/>
+    <title> TEST </title>
+    <meta charset="utf-8" />
 </head>
+
 <body>
-       <?php
-       echo "TEST";
-      ?>
-</body> 
-</html>
+    <?php
+        $nome = Brenda;
+        $idade = 20;
+        $n1 = $_GET["a"];
+        $n2 = $_GET ["b"];
+        $m =($n1 + $n2)/2;
+        echo "$nome tem $idade anos e a média de suas nota foi $m";
+        $sit = ($m < 6)?"VOCÊ FOI REPROVADO":"VOCÊ FOI APROVADO";
+        echo "</br> A situação do aluno é: $sit";
+    ?>
+</body>
+
+</html> 
