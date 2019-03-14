8ba8128ce982d6aa8c4d00199c54a601e66729e1
Gustavo Dauer <gustavo.dauer@webjump.com.br>
2019-03-14 12:18:26 -0300

    Teste TDD Passou acessabdTest

--- gustavo/test/acessabdTest.php
+++ gustavo/test/acessabdTest.php
@@ -10,15 +10,26 @@ use PHPUnit\Framework\TestCase;
 class acessabdTest extends TestCase
 {
 
-    private $host='localhost', $user='root', $password='root', $database='Webjump', $link;
+    private $host, $user, $password, $database, $link;
 
-    protected function testConnect($host, $user, $password, $database, $link)
+    public function __construct($host='', $user='', $password='', $database='', $link=null)
     {
+        $this->host     = $host;
+        $this->user     = $user;
+        $this->password = $password;
+        $this->database = $database;
+        $this->link     = $link;
+    }
 
+
+    public function testConnect($host='localhost', $user='root', $password='root', $database='Webjump')
+    {
         $link = mysqli_connect("$host", "$user", "$password", "$database");
         if(!$link)
         {
             die("Connection has failed :( <br />" . mysqli_connect_error());
         }
+
     }
 }
+
