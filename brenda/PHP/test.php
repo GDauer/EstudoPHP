514bf574a1742e616045df5a10689fa948f6268d
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-14 10:13:22 -0300

    Estrutura básica

--- brenda/PHP/test.php
+++ brenda/PHP/test.php
@@ -1,29 +1,12 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title> TEST </title>
-    <meta charset="utf-8" />
+       <title> TEST </title>
+       <meta charset="utf-8"/>
 </head>
-
 <body>
-    <?php
-    $nome = Brenda;
-    $idade = 20;
-    $n1 = 8;
-    $n2 = 7;
-    $a = $n1 + $n2;
-    $b = $n1 - $n2;
-    $c = $n1 * $n2;
-    $d = $n1 / $n2;
-    $e = $n1 % $n2;
-    echo "$nome tem $idade anos!";
-    echo "<br/><br/> A soma das notas entre $n1 e $n2 é igual a: $a";
-    echo "<br/>  A subtração das notas entre $n1 e $n2 é igual a: $b";
-    echo "<br/>  A multiplicação das notas entre $n1 e $n2 é igual a: $c";
-    echo "<br/>  A divisão das notas entre $n1 e $n2 é igual a: $d";
-    echo "<br/>  O modulo das notas entre $n1 e $n2 é igual a: $e";
-    ?>
-</body>
-
-</html> 
+       <?php
+       echo "TEST";
+      ?>
+</body> 
+</html>
