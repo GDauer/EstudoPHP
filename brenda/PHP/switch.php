5718e465b0ffe235446de8863e02a5a0ea09ff56
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-14 14:50:43 -0300

    Estrutura SWITCH

---
+++ brenda/PHP/switch.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> TEST </title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <?php
+    $n = isset($_GET["num"])?$_GET["num"]:0;
+    $o = isset($_GET ["oper"])?$_GET["oper"]:1;
+    switch ($o) {
+        case 1:
+            $r = $n * 2;
+            break;
+
+        case 2:
+            $r = $n ^ 3;
+            break;
+        case 3:
+            $r = sqrt($n);
+    }
+    echo "O resultado da operação é: $r";
+    ?>
+    <a href="switch.html"> VOLTAR </a>
+</body>
+
+</html> 
