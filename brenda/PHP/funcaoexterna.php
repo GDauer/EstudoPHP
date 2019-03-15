eb595998cfb4864ed21d2e7a1dab94acab04d264
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-15 15:46:09 -0300

    Usando funções externas

---
+++ brenda/PHP/funcaoexterna.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> TEST </title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <?php
+       include "funcoes.php";
+       echo "<h1> Testando novas funções </h1>";
+       ola ();
+       mostravalor(4);
+    ?>
+</body>
+</html> 
