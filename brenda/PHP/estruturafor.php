e58c8aa5165bec2042027efba5324279d67881df
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-15 10:46:29 -0300

    estrutura for

---
+++ brenda/PHP/estruturafor.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> TEST </title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <?php
+    for ($c = 1; $c <= 10; $c++) {
+        echo "$c";
+    }
+    ?>
+</body>
+
+</html> 
