e720ac4056d48622c10eba6898c8731461d3815b
Gustavo Dauer <gustavo.dauer@webjump.com.br>
2019-03-15 12:31:38 -0300

    Teste TDD Passou importTest

---
+++ gustavo/test/importTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gustavo
+ * Date: 15/03/19
+ * Time: 12:25
+ */
+
+class importTest extends crudProdTest
+{
+    private $file;
+
+    public function __construct($file='')
+    {
+        //apenas implementar por último --> diferencial/opcional
+    }
+
+    public function testimportProd()
+    {
+        //methodo de importação de protuduo via .csv
+    }
+
+}
